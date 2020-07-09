6dd4e43efb787e7ea8cb92e47488107e3108bc1e
jeevanthask <jeevanthanandika@gmail.com>
2020-07-09 20:47:07 +0530

    upgrade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import DashboardComponent from "./components/dashboard/dashboard.component";
 
 class App extends React.Component{
 
     render() {
         return(
-            <div>
-                <h1>kcndcdncjd</h1>
-                <p>ncdncnd</p>
-            </div>
+            <BrowserRouter>
+
+                <Switch>
+                    <Route path='/' exact component={DashboardComponent}/>
+                    <Route path='/' render={() => <div>404</div>}/>
+                </Switch>
+
+            </BrowserRouter>
         )
     }
 
---
+++ frontend/src/components/dashboard/dashboard.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class DashboardComponent extends React.Component{
+
+    render() {
+        return(
+            <div>
+                from the dashboard
+            </div>
+        )
+    }
+
+}
+
+export default DashboardComponent
