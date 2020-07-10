73df8e05f6cacc74b18b84fb4e8a3d10f8af1ff6
jeevanthask <jeevanthanandika@gmail.com>
2020-07-10 09:04:30 +0530

    upgrade

--- frontend/src/components/addpatient/addpatient.component.jsx
+++ frontend/src/components/addpatient/addpatient.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import style from './addpatient.module.css'
+import axios from 'axios'
 import {Button, Modal, Form} from "react-bootstrap";
 
 class AddpatientComponent extends React.Component {
@@ -7,7 +8,10 @@ class AddpatientComponent extends React.Component {
     constructor() {
         super();
         this.state = {
-            show: false
+            show: false,
+            firstname:'',
+            lastname:'',
+            email:''
         }
     }
 
@@ -23,12 +27,46 @@ class AddpatientComponent extends React.Component {
         })
     }
 
+    onChangeFirstName = (e) => {
+        this.setState({
+            firstname:e.target.value
+        })
+    }
+
+    onChangeLastName = (e) => {
+        this.setState({
+            lastname:e.target.value
+        })
+    }
+
+    onChangeEmail = (e) => {
+        this.setState({
+            email:e.target.value
+        })
+    }
+
     onSubmit = (e) => {
         e.preventDefault()
 
         this.setState({
             show:false
         })
+
+        const newPatient = {
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            email:this.state.email
+        }
+
+        axios.post('http://localhost:4000/HPSvthree/admin/addpatient',newPatient)
+            .then(res => console.log(res.data))
+
+        this.setState({
+            firstname:'',
+            lastname:'',
+            email:''
+        })
+
     }
 
     render() {
@@ -49,17 +87,17 @@ class AddpatientComponent extends React.Component {
                         <Form onSubmit={this.onSubmit}>
                             <Form.Group>
                                 <Form.Label>First Name</Form.Label>
-                                <Form.Control type="text"/>
+                                <Form.Control type="text" onChange = {this.onChangeFirstName}/>
                             </Form.Group>
 
                             <Form.Group>
                                 <Form.Label>Last Name</Form.Label>
-                                <Form.Control type="text"/>
+                                <Form.Control type="text" onChange = {this.onChangeLastName}/>
                             </Form.Group>
 
                             <Form.Group>
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email"/>
+                                <Form.Control type="email" onChange = {this.onChangeEmail}/>
                             </Form.Group>
 
                             <Modal.Footer>
