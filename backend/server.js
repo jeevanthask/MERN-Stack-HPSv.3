4f53da409ecde7489c56107b136cab9653328a65
jeevanthask <jeevanthanandika@gmail.com>
2020-07-10 07:39:28 +0530

    upgrade

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const PORT = 4000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/HPSvthree', {useNewUrlParser: true})
+const connection = mongoose.connection
+
+connection.once('open', function () {
+    console.log('mongodb connection established successfully')
+})
+
+app.use('/HPSvthree/admin',require('./routes/patients/addpatient.routes'))
+app.use('/HPSvthree/admin',require('./routes/patients/getpatient.routes'))
+app.use('/HPSvthree/admin',require('./routes/patients/updatepatient.routes'))
+app.use('/HPSvthree/admin',require('./routes/patients/deletepatient.routes'))
+
+
+app.listen(PORT, function () {
+    console.log('server is running in port ' + PORT)
+})
