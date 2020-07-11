7ddbe19c6795320ea292fdf05c3b2878d30066ef
jeevanthask <jeevanthanandika@gmail.com>
2020-07-11 17:59:58 +0530

    upgrade

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ app.use('/HPSvthree/admin',require('./routes/symptoms/getsymptom.routes'))
 app.use('/HPSvthree/admin',require('./routes/symptoms/updatesymptom.routes'))
 app.use('/HPSvthree/admin',require('./routes/symptoms/deletesymptom.routes'))
 
+app.use('/HPSvthree/admin',require('./routes/symptoms/addsymptom.routes'))
+
 
 app.listen(PORT, function () {
     console.log('server is running in port ' + PORT)
