147410735ffae253b6799161cb07ba827f824584
jeevanthask <jeevanthanandika@gmail.com>
2020-07-12 23:21:35 +0530

    upgrade

--- frontend/src/components/charts/doctorproficiency/doctorproficiencychart.component.jsx
+++ frontend/src/components/charts/doctorproficiency/doctorproficiencychart.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Bar, Bubble, Line, Pie, Polar, Radar} from 'react-chartjs-2';
 import axios from "axios";
-import {Form} from "react-bootstrap";
 
 class DoctorproficiencychartComponent extends React.Component {
 
@@ -10,34 +9,18 @@ class DoctorproficiencychartComponent extends React.Component {
 
         this.state = {
             doctors: [],
-            chartData: {
-                labels: ['Colombo', 'Kalutara', 'Gampaha', 'Puttalam', 'Jaffna',],
-                datasets: [
-                    {
-                        label: 'populattion',
-                        data: [155, 63, 41, 39, 16],
-                        backgroundColor: [
-                            '#f50057',
-                            '#ff6d00',
-                            '#33691e',
-                            '#0091ea',
-                            '#76ff03'
-                        ],
-                        borderColor: [],
-                        borderWidth: 0
-                    }
-                ]
-            },
-            Pediatrician:'',
-            Obstetrician: '',
-            Surgeon: '',
-            Psychiatrist: '',
-            Cardiologist: '',
-            Dermatologist: '',
-            Endocrinologist: '',
-            Gastroenterologist: '',
-            Nephrologist: '',
-            Ophthalmologist: ''
+
+            Pediatrician: 0,
+            Obstetrician: 0,
+            Surgeon: 0,
+            Psychiatrist: 0,
+            Cardiologist: 0,
+            Dermatologist: 0,
+            Endocrinologist: 0,
+            Gastroenterologist: 0,
+            Nephrologist: 0,
+            Ophthalmologist: 0
+
         }
     }
 
@@ -54,8 +37,116 @@ class DoctorproficiencychartComponent extends React.Component {
 
     }
 
+    generateChartData = () => {
+        return (
+            {
+                labels: [
+                    'Pediatrician', 'Obstetrician', 'Surgeon', 'Psychiatrist', 'Cardiologist',
+                    'Dermatologist', 'Endocrinologist', 'Gastroenterologist', 'Nephrologist', 'Ophthalmologist'
+                ],
+                datasets: [
+                    {
+                        label: 'Speciality',
+                        data: [
+                            this.state.Pediatrician,
+                            this.state.Obstetrician,
+                            this.state.Surgeon,
+                            this.state.Psychiatrist,
+                            this.state.Cardiologist,
+                            this.state.Dermatologist,
+                            this.state.Endocrinologist,
+                            this.state.Gastroenterologist,
+                            this.state.Nephrologist,
+                            this.state.Ophthalmologist
+                        ],
+                        backgroundColor: [
+                            '#f50057',
+                            '#ff6d00',
+                            '#33691e',
+                            '#0091ea',
+                            '#76ff03',
+                            '#f50057',
+                            '#ff6d00',
+                            '#33691e',
+                            '#0091ea',
+                            '#76ff03'
+                        ],
+                        borderColor: [],
+                        borderWidth: 0
+                    }
+                ]
+            }
+        )
+    }
+
+
     processMaximumSpeciality = () => {
 
+        for (let i = 0; i < this.state.doctors.length; i++) {
+
+
+            if (this.state.doctors[i].speciality === 'Pediatrician') {
+                this.setState({
+                    Pediatrician: this.state.Pediatrician++
+                })
+            }
+            if (this.state.doctors[i].speciality === 'Obstetrician') {
+                this.setState({
+                    Obstetrician: this.state.Obstetrician++
+                })
+            }
+            if (this.state.doctors[i].speciality === 'Surgeon') {
+
+                this.setState({
+                    Surgeon: this.state.Surgeon++
+                })
+            }
+            if (this.state.doctors[i].speciality === 'Psychiatrist') {
+
+                this.setState({
+                    Psychiatrist: this.state.Psychiatrist++
+                })
+            }
+            if (this.state.doctors[i].speciality === 'Cardiologist') {
+
+                this.setState({
+                    Cardiologist: this.state.Cardiologist++
+                })
+            }
+            if (this.state.doctors[i].speciality === 'Dermatologist') {
+
+                this.setState({
+                    Dermatologist: this.state.Dermatologist++
+                })
+            }
+            if (this.state.doctors[i].speciality === 'Endocrinologist') {
+
+                this.setState({
+                    Endocrinologist: this.state.Endocrinologist++
+                })
+            }
+            if (this.state.doctors[i].speciality === 'Gastroenterologist') {
+
+                this.setState({
+                    Gastroenterologist: this.state.Gastroenterologist++
+                })
+            }
+            if (this.state.doctors[i].speciality === 'Nephrologist') {
+
+                this.setState({
+                    Nephrologist: this.state.Nephrologist++
+                })
+            }
+            if (this.state.doctors[i].speciality === 'Ophthalmologist') {
+
+                this.setState({
+                    Ophthalmologist: this.state.Ophthalmologist++
+                })
+            }
+
+        }
+
+        alert(this.state.Gastroenterologist)
 
     }
 
@@ -66,7 +157,7 @@ class DoctorproficiencychartComponent extends React.Component {
                     <div className="alert alert-success" role="alert">
                         <h4>Doctor Proficiency</h4>
                         <Bar
-                            data={this.state.chartData}
+                            data={this.generateChartData()}
                             options={{
                                 title: {
                                     display: true,
@@ -81,6 +172,8 @@ class DoctorproficiencychartComponent extends React.Component {
                             }}
                         />
                     </div>
+
+                    <button className="btn btn-primary" onClick={this.processMaximumSpeciality}>Process</button>
 
                 </div>
             </div>
